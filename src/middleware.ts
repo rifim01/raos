f607b768ec8b2aaf66a926e29b582025945436cf
rifim01 <rifim01@adminrifim.org>
2026-06-18 14:03:32 +0800

    fix: middleware - redirect login user ke / bukan /dashboard

--- src/middleware.ts
+++ src/middleware.ts
@@ -55,7 +55,7 @@ export async function middleware(request: NextRequest) {
 
   if (user && pathname === "/login") {
     const url = request.nextUrl.clone();
-    url.pathname = "/dashboard";
+    url.pathname = "/";
     return NextResponse.redirect(url);
   }
 
